a444c540df52f70affa7c30236dd43a93ee54bd3
sidude2003 <siddharth.devaraju@gmail.com>
2023-01-10 14:35:29 +0530

    first-commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-sm navbar-light" id="neubar">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src="download (1).png" height="60" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className=" collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav ms-auto ">
+              <li className="nav-item">
+                <a
+                  className="nav-link mx-2 active text-light"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link mx-2 text-light" href="#">
+                  Our Team
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link mx-2 text-light" href="#">
+                  Projects
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link mx-2 dropdown-toggle text-light"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Company
+                </a>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Blog
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Contact us
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
